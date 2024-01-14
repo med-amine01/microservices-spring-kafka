972f4c8ec54b52c0e656e985fc099c6f19620d33
med-amine01 <m.chebbi.tech@gmail.com>
2024-01-14 12:00:11 +0100

    stock-service consume from order-service

---
+++ stock-service/src/main/java/m/chebbi/tech/stockservice/kafka/OrderConsumer.java
@@ -0,0 +1,14 @@
+package m.chebbi.tech.stockservice.kafka;
+
+import m.chebbi.tech.basedomains.dto.OrderEvent;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderConsumer {
+
+    @KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+    public void consume(OrderEvent event) {
+        System.err.printf("Order EVENT received => %s", event.toString());
+    }
+}
